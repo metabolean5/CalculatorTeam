4a279ca0fde6a6796c7a5412c85a5f667ef5d25a
Keven <presseauk@gmail.com>
2019-06-06 14:20:07 -0400

    fixes and moved

--- Test.java
+++ Test.java
@@ -1,2 +1,30 @@
 public class Test {
+	
+	public static void TestCosh(){
+	Boolean coshtest = true;
+	int testCount = 10000;
+	if(Calculator.Cosh(0) != 1)
+		coshtest = false;
+	for(int i = 0; i < testCount; i++){
+		double x = 10000 - 20000*Math.random();
+		double hcosh = Calculator.Cosh(x);
+		double fcosh = Math.cosh(x);
+			
+		if(CheckPrecision(hcosh, fcosh)){
+			coshtest = false;
+			System.out.println("Cosh Fail: " + x + " / " 
+						+ Double.toString(hcosh) + " != " + Double.toString(fcosh)); 
+			}
+		}
+	System.out.println("Cosh function with a precision of 10^-10 has passed the test: " + coshtest);
+	}
+	public static Boolean CheckPrecision(double hcosh, double fcosh){
+		double prec = 10E-10;
+		double dif = hcosh - fcosh;
+		if(dif < 0)
+			dif = -dif;
+		return (dif > prec);
+	}
 }
+
+
