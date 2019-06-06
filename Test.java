2f54c018ea3ef9b48bb6a0b1be94d050d48b0ad4
Markisino <tropiano.m@gmail.com>
2019-06-06 14:32:53 -0400

    Added Test Function for 10^x

--- Test.java
+++ Test.java
@@ -1,2 +1,14 @@
 public class Test {
+
+    public static boolean CheckTenPower(float exponent){
+        double x = Calculator.TenPower(exponent);
+        double y = Math.pow(10, exponent);
+
+        System.out.println("Calculator.TenPower: " + x + "\tMath.pow: " + y);
+
+        if (x == y)
+            return true;
+
+        return false;
+    }
 }
