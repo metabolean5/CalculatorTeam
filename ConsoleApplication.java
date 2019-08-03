2563486aa8ef8c3ced8eb55845d233a676d1f020
Markisino <tropiano.m@gmail.com>
2019-08-03 13:59:39 -0400

    Removed 10^x in console

--- ConsoleApplication.java
+++ ConsoleApplication.java
@@ -13,7 +13,7 @@ public class ConsoleApplication {
 
 	
 		while(!exit) {
-			System.out.println("POSSIBLE FUNCTIONS: \n\n -exp(x) \t -cosh(x) \n\n -sqrt(x) \t -10^x\n\n -sin(x) \n\n -pow(x) \n\n " );
+			System.out.println("POSSIBLE FUNCTIONS: \n\n -exp(x) \t -cosh(x) \n\n -sqrt(x)\t -sin(x) \n\n -pow(x) \n\n " );
 			System.out.println("Enter Calculations (type 'exit' to end): " );
 		
 			String str= scan.nextLine();
